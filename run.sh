8ccdd93b4370c1a9346b32772b5bfed2400ba77d
Diogo <diogo.asouza@gmail.com>
2018-02-14 10:21:00 +0000

    fix to the run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 set -e
 # Sleep to let the db server come up
 sleep 5
+
+mix deps.get
+mix deps.compile
 cd apps/bank_web/web/static/assets/
 npm install
 cd ../../../../../
-mix deps.get
-mix deps.compile
 mix ecto.create
 mix ecto.migrate
 mix phx.server
